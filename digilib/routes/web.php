b0bb18c3f87adb12be4fb5e1b3eab60fdd569ab1
karismanabil <kharismanabil@gmail.com>
2022-01-02 16:08:13 +0700

    dashboard 1

--- digilib/routes/web.php
+++ digilib/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/detailbooks', function () {
     return view('detailbooks');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
